d7466674a0d2049db7b89f5fed2c6ce0db2cd268
amrutha.d1 <amrutha.ad78@gmail.com>
2023-06-07 15:52:34 +0530

    ui

---
+++ src/components/Navbar.js
@@ -0,0 +1,105 @@
+import { Mail, Notifications, Pets } from "@mui/icons-material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+  styled,
+} from "@mui/material";
+import React, { useState } from "react";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: "flex",
+  gap: "15px",
+  alignItems: "center",
+
+  [theme.breakpoints.up("xs")]: {
+    display: "none",
+  },
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+const Userbox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  gap: "10px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          Pet Shop
+        </Typography>
+        <Pets sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="Search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={2} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6pV4YQt29S3YV2WNMGi4bKNUe330d03aodQ&usqp=CAU"
+            sx={{ width: "30px", height: "30px" }}
+            onClick={(e) => setOpen(true)}
+          />
+        </Icons>
+        <Userbox onClick={(e) => setOpen(true)}>
+          <Avatar
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6pV4YQt29S3YV2WNMGi4bKNUe330d03aodQ&usqp=CAU"
+            sx={{ width: "30px", height: "30px" }}
+          />
+          <Typography variant="span">Amrutha</Typography>
+        </Userbox>
+      </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={(e) => setOpen(false)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </AppBar>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Rightbar.js
@@ -0,0 +1,165 @@
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  Divider,
+  ImageList,
+  ImageListItem,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import React from "react";
+
+const Rightbar = () => {
+  return (
+    <Box
+      flex={2}
+      p={2}
+      sx={{
+        display: {
+          xs: "none",
+          sm: "block",
+        },
+      }}
+    >
+      <Box position="fixed" width={300}>
+        <Typography variant="h6" fontWeight={100}>
+          Online Friends
+        </Typography>
+        <AvatarGroup max={4}>
+          <Avatar
+            alt="Remy Sharp"
+            src="https://material-ui.com/static/images/avatar/1.jpg"
+          />
+
+          <Avatar alt="Amrutha" src="" />
+          <Avatar
+            alt="Travis Howard"
+            src="https://material-ui.com/static/images/avatar/2.jpg"
+          />
+          <Avatar
+            alt="Cindy Baker"
+            src="https://material-ui.com/static/images/avatar/3.jpg"
+          />
+          <Avatar
+            alt="Agnes Walker"
+            src="https://material-ui.com/static/images/avatar/4.jpg"
+          />
+          <Avatar
+            alt="Trevor Henderson"
+            src="https://material-ui.com/static/images/avatar/5.jpg"
+          />
+          <Avatar alt="Shwetha" src="" />
+        </AvatarGroup>
+        <Typography varaint="h6" fontWeight={100} mt={2}>
+          Latest Photos
+        </Typography>
+        <ImageList cols={3} rowHeight={100} gap={5}>
+          <ImageListItem>
+            <img
+              src="https://images.news18.com/ibnlive/uploads/2022/08/cat-16598747213x2.jpg?impolicy=website&width=360&height=240"
+              alt=""
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="https://i.insider.com/5df126b679d7570ad2044f3e?width=700"
+              alt=""
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="https://play-lh.googleusercontent.com/AmKSpZt_rynhOO0ID1eS0gqeW3DFzoH6KNZkAAgepQ0t9MDRQTmil-nlY5GqkZ_7El0"
+              alt=""
+            />
+          </ImageListItem>
+        </ImageList>
+
+        <Typography varaint="h6" fontWeight={100}>
+          Latest Conversations
+        </Typography>
+        <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar
+                alt="Remy Sharp"
+                src="https://material-ui.com/static/images/avatar/1.jpg"
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Brunch this weekend?"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Ali Connors
+                  </Typography>
+                  {" — I'll be in your neighborhood doing errands this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar
+                alt="Travis Howard"
+                src="https://material-ui.com/static/images/avatar/2.jpg"
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Summer BBQ"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    to Scott, Alex, Jennifer
+                  </Typography>
+                  {" — Wish I could come, but I'm out of town this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar
+                alt="Cindy Baker"
+                src="https://material-ui.com/static/images/avatar/3.jpg"
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Oui Oui"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Sandra Adams
+                  </Typography>
+                  {" — Do you have Paris recommendations? Have you ever…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+        </List>
+      </Box>
+    </Box>
+  );
+};
+
+export default Rightbar;
